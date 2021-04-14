e6f43a98787693ddf849c9d1d6e0bd0b6447e4ec
pedrolgsoares <pedrodevmob@gmail.com>
2021-04-14 18:51:41 -0300

    Design versao 1.0 realizada

--- app/src/main/java/com/pedrolgsoares/conversordemoedas/MainActivity.java
+++ app/src/main/java/com/pedrolgsoares/conversordemoedas/MainActivity.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+//    O onCreate:  É a primeira função a ser executada em uma Activity.
+//    Geralmente é a responsável por carregar os layouts XML e outras operações de inicialização.
+//    É executada apenas uma vez
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//   Esse método é responsável por configurar o layout XML.
+//   Esse método normalmente é chamado no método onCreate para a criação da interface.
+//   Sendo a classe R, responsável pelos recursos Ou seja toda vez que o android executar o onCreate
+//   vai setar o layout no activity_main
         setContentView(R.layout.activity_main);
     }
 }
