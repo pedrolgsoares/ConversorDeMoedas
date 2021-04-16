2001594920da4dbc34d3724dfab975ced1f1b098
pedrolgsoares <pedrodevmob@gmail.com>
2021-04-16 10:13:12 -0300

    Logica da conversao criada

--- app/src/main/java/com/pedrolgsoares/conversordemoedas/MainActivity.java
+++ app/src/main/java/com/pedrolgsoares/conversordemoedas/MainActivity.java
@@ -1,11 +1,18 @@
-   package com.pedrolgsoares.conversordemoedas;
+package com.pedrolgsoares.conversordemoedas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+// Implementando a interface do OnClickListener
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//    Instanciando a classe ViewHolder fora do onCreate
+    private Viewholder mviewHolder = new Viewholder();
     @Override
 //    O onCreate:  É a primeira função a ser executada em uma Activity.
 //    Geralmente é a responsável por carregar os layouts XML e outras operações de inicialização.
@@ -17,5 +24,48 @@ public class MainActivity extends AppCompatActivity {
 //   Sendo a classe R, responsável pelos recursos Ou seja toda vez que o android executar o onCreate
 //   vai setar o layout no activity_main
         setContentView(R.layout.activity_main);
+
+//        Agora instanciar os atributos abaixo do setContentView
+        this.mviewHolder.valorBR = findViewById(R.id.valorBR);
+        this.mviewHolder.valorUSD = findViewById(R.id.valorUSD);
+        this.mviewHolder.converter = findViewById(R.id.converter);
+        this.mviewHolder.sairApp = findViewById(R.id.sairApp);
+
+//        Instanciando os atributos dos botões com suas funções
+        this.mviewHolder.converter.setOnClickListener(this);
+        this.mviewHolder.sairApp.setOnClickListener(this);
+
+        this.limpar();
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.converter){
+            String valor = this.mviewHolder.valorBR.getText().toString();
+            if("".equals(valor)){
+                Toast.makeText(this, "Informe o valor para a conversão", Toast.LENGTH_LONG).show();
+            } else {
+//                Implementar API para cotação automática
+                Double valorReal = Double.valueOf(valor);
+                this.mviewHolder.valorUSD.setText(String.format("%.2f", valorReal*5.62));
+            }
+        }else{
+//            Para sair do app
+        }
+    }
+
+    public void limpar(){
+        this.mviewHolder.valorBR.setText("");
+        this.mviewHolder.valorUSD.setText("");
+    }
+
+
+    //    Criando a classe padrão ViewHolder
+    private static class Viewholder{
+        EditText valorBR;
+        TextView valorUSD;
+        Button converter;
+        Button sairApp;
+    }
+
 }
